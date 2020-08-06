d7da0287f38473e07f3830315af06e9368157e7f
Bec Sun <rebeccasun86@gmail.com>
2020-08-06 22:40:49 +0100

    styling and working on the funtion for the ingredients

--- src/components/cocktails/Cocktail.js
+++ src/components/cocktails/Cocktail.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import axios from 'axios'
+import check from '../common/images/check.png'
+import cross from '../common/images/cross.png'
 class Cocktail extends React.Component{
   state={
     booze: null,
     message: ' ',
-    ingredients: [],
-    measurements: []
+    measurements: ' ',
   }
   async componentDidMount() {
     try {
@@ -17,37 +18,38 @@ class Cocktail extends React.Component{
   getDrink = async() => {
     try {
       const res = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-      console.log({ booze: res.data })
+      // console.log({ booze: res.data })
       this.setState({ booze: res.data })
-      this.setState({ message: 'All you need to know: It has ' +  this.state.booze.drinks[0].strIngredient1 })
-      this.setState({ ingredients: this.state.booze.drinks[0].strMeasure1) })
+      this.setState({ message: 'All you need to know: It has ' +  this.state.booze.drinks[0].strIngredient1 
+      + ' and ' +  this.state.booze.drinks[0].strIngredient2 })
     } catch (err){
       console.log('an error has occured'  + err)
     }
-  } 
+  }
   clickDrink = () => {
+    this.setState({ measurements: '' })
     return (this.getDrink())
   }
   getRecipe = () => {
+    // this is a cheeky placeholder after all the failed maps, filters and for loops
     this.setState({ message: this.state.booze.drinks[0].strInstructions })
+    this.setState({ measurements: this.state.booze.drinks[0].strIngredient1 + ' ' + this.state.booze.drinks[0].strMeasure1 + 
+    ' , ' +  this.state.booze.drinks[0].strIngredient2 + ' ' + this.state.booze.drinks[0].strMeasure2 + ' , '})
   }
-  getIngredients = () => {
-    this.setState({ ingredients: this.state.ingredients.push(this.state.booze.drinks[0].strIngredient1)})
-  }
-
   render(){
     if (!this.state.booze) return null
-    console.log(this.state.ingredients)
     return (
       <div className="homestyle">
         <div className="cocktailCard">
           <div className="innercard">
             <div><img src={this.state.booze.drinks[0].strDrinkThumb}></img></div>
             <div className="cocktailText">
-              <button onClick={this.clickDrink}>NEXT</button>
-              <button onClick={this.getRecipe}>Recipe</button>
-              <div><h1>{this.state.booze.drinks[0].strDrink}</h1></div>
-              <div><h4>{this.state.message}</h4></div>
+              <div><h2>{this.state.booze.drinks[0].strDrink}</h2></div>
+              <div><h4>{this.state.message}<p>{this.state.measurements}</p></h4></div>
+              <div className="buttonDiv">
+                <img className ="buttonImage" src={cross} width="50" height="50" onClick={this.clickDrink}></img>
+                <img className ="buttonImage" src={check} width="50" height="50"  onClick={this.getRecipe}></img>
+              </div>
             </div>
           </div>
         </div>
--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 const Home = () => {
   return (<div className="homestyle">
     <h1>ALCOHOL U LATER</h1>
+    <h3> "Your cocktail wing-man" </h3>
   </div>)
 }
 export default Home
