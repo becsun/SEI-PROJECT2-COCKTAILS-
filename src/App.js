693148c610c9295b5a42120fb3cde6247010aa25
Bec Sun <rebeccasun86@gmail.com>
2020-08-06 10:22:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+console.log(process.env.REACT_APP_MY_API_KEY)
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <h1>Hello World2</h1>
+    
+    
   )
 }
 
